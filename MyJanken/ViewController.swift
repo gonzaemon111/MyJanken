5af364a9b1c39aa07c25c6caaef31d90a541af9e
GokiSakuraba <goki14914@icloud.com>
2018-12-16 20:54:59 +0900

    add ViewController for Random number

--- MyJanken/ViewController.swift
+++ MyJanken/ViewController.swift
@@ -15,14 +15,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    var answerNumber = 0
+    
 
     @IBOutlet weak var answerImageView: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
     @IBAction func shuffleAction(_ sender: Any) {
-        answerLabel.text = "グー"
-        answerImageView.image = UIImage(named: "gu")
+        // arc4random_uniform()の戻り値は、UInt32。
+        // Int型じゃないからキャストしてInt型に変更。
+        answerNumber = Int(arc4random_uniform(3))
+        if answerNumber == 0 {
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named: "gu")
+        } else if answerNumber == 1 {
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named: "choki")
+        } else if answerNumber == 2 {
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named: "pa")
+        }
     }
-    
 
 }
 
